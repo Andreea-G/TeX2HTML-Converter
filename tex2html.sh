3145c4be5c799aab2db6360319832487676b45b7
Andreea_G <a.georgescu@physics.ucla.edu>
2014-10-26 20:49:05 -0700

    Cleaning up is only done if mk4ht is run

--- tex2html.sh
+++ tex2html.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TEXFILE=$1
+FILE=$1
+FOLDER=$2
+TEXFILE=$FOLDER/$FILE/$FILE.tex
 INFILE=${TEXFILE%.tex}.xht
 OUTFILE=${TEXFILE%.tex}.html
 CSSFILE=${TEXFILE%.tex}.css
@@ -12,26 +14,19 @@ if [ $RERUNMK4HT -eq 1 ]; then
   echo "--- Running mk4ht ---"
   mk4ht mzlatex $TEXFILE "html,mathplayer" #> /dev/null
   cp $CSSFILE $CSSFILE1  #store for backup later
+  # Clean up
+  cd $FOLDER/$FILE
+  \rm $FILE.4ct $FILE.4tc $FILE.dvi $FILE.idv $FILE.lg $FILE.tmp $FILE.xref #Do not delete $FILE.toc, or the contents will not appear!
+  cd ..
+  cd ..
 else
   cp $CSSFILE1 $CSSFILE
 fi
-
-mv $FILE.* $FOLDER/$FILE/
-mv $FILE*x.png $FOLDER/$FILE/
 
 echo
 echo "--- Running xht2html ---"
 ./xht2html $INFILE $OUTFILE $CSSFILE
 
-# Clean up
-cd $FOLDER/$FILE
-\rm $FILE.4ct $FILE.4tc $FILE.dvi $FILE.idv $FILE.lg $FILE.tmp $FILE.xref   #Do not delete $FILE.toc, or the contents will not appear!
-cd ..
-cd ..
 
-echo DONE
-
-#kill $(ps -A | grep -m1 firefox | awk '{print $1}')
-#/Applications/Firefox.app/Contents/MacOS/firefox $OUTFILE &
-
+echo Done.
 
