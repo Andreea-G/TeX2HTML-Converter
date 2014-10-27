a3240cf7c9ea3e4b607fbd0a3b620ca3ed2eb880
Andreea_G <a.georgescu@physics.ucla.edu>
2014-10-26 21:27:16 -0700

    Made tex2html script take only one argument, the name of the texfile. Also added tex preamble and html_commands

---
+++ install.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+install_dir=${HOME}/.tex2html
+
+message_if_failed() {
+    #print out message and quit if last command failed
+    if [ $? -ne 0 ]; then 
+        echo -e >&2 $1
+        exit 2
+    fi
+}
+
+command -v mk4ht >/dev/null 2>&1 && has_mk4ht=true || has_mk4ht=false
+command -v hg >/dev/null 2>&1 && has_hg=true || has_hg=false
+echo "#include <re2/re2.h>" > delete_this__checking_for_re2_header.h 
+if g++ -E delete_this__checking_for_re2_header.h; then
+	has_re2_header=true
+else
+	has_re2_header=false
+fi
+\rm delete_this__checking_for_re2_header.h
+
+# Notify user if there are any missing dependencies
+programs_to_install=""
+if [ "$has_mk4ht" = true ]; then
+	programs_to_install="${programs_to_install} tex4ht"
+	if [ "$has_hg" = true ]; then
+		programs_to_install="${programs_to_install} mercurial"
+	fi
+fi
+if [ "$has_re2_header" = true ]; then
+	programs_to_install="${programs_to_install} re2"
+fi
+
+
+if [ "$programs_to_install" != "" ]; then
+	echo "The following dependencies are not met: $programs_to_install"
+	read -p "Would you like to install them now? (y/n)  " user_input
+    case $user_input in
+        [Yy]* )
+     		echo "Installing programs...  YAYYY!"
+			#TODO: check for admin priveledges here
+        [Nn]* )
+            echo "Ok, fine!  Be that way.  I never liked you anyway.  You can reinstall those programs manually and rerun this script."
+			echo "Exitting..."
+			exit 4
+        * )
+            echo "Input not understood.  Please type 'y' or 'n' next time."
+			echo "Exitting..."
+			exit 5
+    esac
+fi
+
+if [ "$has_re2" = "false" ]; then
+	if [ "$has_hg" = "false" ]; then
+		apt-get install mercurial #TODO: platform independence
+		message_if_failed "Failed to install mercurial!"
+	fi
+	hg clone https://re2.googlecode.com/hg .temporary_re2
+	pushd .temporary_re2
+	make test
+	make install 
+	# re2 has an unresolved bug. This is a workaround until it gets fixed.   #TODO: provide link to the bug
+	sed -i -e 's/f.FirstMatch/\/\/f.FirstMatch/g' 
+	make testinstall
+	popd
+	\rm .temporary_re2
+fi
+
+if [ "$has_mk4ht" = "false" ]; then
+	apt-get install tex4ht  #TODO: make platform indepedent
+	message_if_failed "Failed to install tex4ht.  Major bummer, dude!"
+fi
+
+
+if [ -d  $install_dir ]; then
+	echo >&2 "The folder $install_dir already exists!  Whhoaaa boy!  Please delete it first and try reinstalling."
+	exit 3
+fi
+
+echo "Downloading tex2html..."
+git clone https://Garrett_R@bitbucket.org/Andreea_G/tex2html.git -o $install_dir
+message_if_failed "Failed to download tex2html..."
+pushd $install_dir
+echo "Installing..."
+make
+
+popd
+
+
+# We now add the executable to the user's path
+cat >> ${HOME}/.profile << END
+
+# Allow user to execute tex2html from anywhere (this block was added by tex2html's install script)
+if [ -d "$install_dir" ] ; then
+    PATH="$install_dir:\$PATH"
+fi
+
+END
+
+
+echo "Done!"
+
--- tex2html.sh
+++ tex2html.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-FILE=$1
-FOLDER=$2
-TEXFILE=$FOLDER/$FILE/$FILE.tex
-INFILE=${TEXFILE%.tex}.xht
-OUTFILE=${TEXFILE%.tex}.html
-CSSFILE=${TEXFILE%.tex}.css
-CSSFILE1=${TEXFILE%.tex}_1.css
-#echo $TEXFILE
+TEXFILE=$1
+FILE=${TEXFILE%.tex}
+INFILE=$FILE.xht
+OUTFILE=$FILE.html
+CSSFILE=$FILE.css
+CSSFILE1=$FILE'_1'.css
+echo $TEXFILE
+echo $HTMLFILE
+echo $CSSFILE
+echo $CSSFILE1
 
-RERUNMK4HT=0
+
+RERUNMK4HT=1
 if [ $RERUNMK4HT -eq 1 ]; then
   echo "--- Running mk4ht ---"
   mk4ht mzlatex $TEXFILE "html,mathplayer" #> /dev/null
   cp $CSSFILE $CSSFILE1  #store for backup later
   # Clean up
-  cd $FOLDER/$FILE
   \rm $FILE.4ct $FILE.4tc $FILE.dvi $FILE.idv $FILE.lg $FILE.tmp $FILE.xref #Do not delete $FILE.toc, or the contents will not appear!
-  cd ..
-  cd ..
 else
   cp $CSSFILE1 $CSSFILE
 fi
